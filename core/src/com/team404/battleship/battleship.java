40fd0e2c42b831aaf5718aa11daeaaad3557d8a9
vux.nguyen <19127632@student.hcmus.edu.vn>
2022-03-31 23:10:18 +0700

    remake project

--- core/src/com/team404/battleship/battleship.java
+++ core/src/com/team404/battleship/battleship.java
@@ -1,11 +1,12 @@
 package com.team404.battleship;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class battleship extends ApplicationAdapter {
+public class battleship extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
